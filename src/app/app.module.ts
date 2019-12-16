03a76fe7b0e711f18a2bd6cd18dc9f92e93c1cce
bahrini544 <abahrini544@gmail.com>
2019-12-17 00:02:16 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Act1Component } from './act1/act1.component';
-import { CalculatriceComponent } from './calculatrice/calculatrice.component';
-import { FormsModule }   from '@angular/forms';
-import { Act2Component } from './act2/act2.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import {FormsModule} from '@angular/forms' ; 
 @NgModule({
   declarations: [
     AppComponent,
-    Act1Component,
-    CalculatriceComponent,
-    Act2Component,
     HomeComponent,
     AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
-    
+    AppRoutingModule, 
+  FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
